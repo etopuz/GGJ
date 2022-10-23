882c19e3d3e4b888554aee42cd851ce8fa9c88bc
mybilge <mehmetyusufbilge@gmail.com>
2022-10-23 16:07:35 +0300

    aef

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource audioData;
+    [SerializeField] AudioClip[] audios;
+    private void Start() {
+        
+        audioData = GetComponent<AudioSource>();
+        audioData.clip = audios[0];
+        audioData.loop = true;
+        audioData.Play();
+    }
+
+    public void GameStart()
+    {
+        audioData.Stop();
+        
+        audioData.clip = audios[1];
+        audioData.loop = true;
+        audioData.Play();
+    }
+}
